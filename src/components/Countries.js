5a6d49c1800d53f440f4c447f5de5bbc79b7a6e5
kichalomex <kichalo23con@hotmail.com>
2023-03-07 18:33:07 -0600

    Paso 2
    Paso 2 paso 2.13

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,14 +1,21 @@
+import { useState } from "react"
 import CountryData from "./CountryData"
 
 const Countries = ({ countriesShow }) => {
+    const [country, setCountry] = useState("")
+
     if (countriesShow.length === 1) {
         return <CountryData country={countriesShow[0]} />
     } else if (countriesShow.length <= 10) {
         return (
             <div>
                 {countriesShow.map((country) => (
-                    <div key={country.name.official}>{country.name.common}</div>
+                    <div key={country.name.official}>
+                        {country.name.common}{" "}
+                        <button onClick={() => setCountry(country)}>show</button>
+                        </div>
                 ))}
+                {country ? <CountryData country={country} /> : null }
             </div>
         )
     } else if (countriesShow.length > 10) {
